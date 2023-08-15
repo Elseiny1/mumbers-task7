9c2a8185fb2f9caf5d63ad1c4390b4b7fea5fd61
EAGLE <EAGLE@DESKTOP-24AJO1U>
2023-08-15 19:03:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace numbers
+{
+    public class program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter number: ");
+            int number=int.Parse(Console.ReadLine()); 
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(number);
+            }
+            
+        }
+    }
+}
